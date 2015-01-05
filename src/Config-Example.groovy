8a946224acb4af86da35c86103b2dfb8b0455ca2
Hubbitus <pahan@hubbitus.info>
2015-01-06 02:36:41 +0300

    Implement gmailTrueDeleteMessages operation!

--- src/Config-Example.groovy
+++ src/Config-Example.groovy
@@ -3,6 +3,8 @@ import info.hubbitus.imaptree.config.ImapAccount
 import info.hubbitus.imaptree.ImapTreeSize
 import info.hubbitus.imaptree.config.Operation
 
+import javax.mail.Flags
+import javax.mail.Folder
 import javax.mail.Message
 
 // By default Config.groovy used as configuration! So, rename this file first, or adjust path in example scripts accordingly!!!
@@ -46,7 +48,7 @@ config{
 			folderProcess: {Node node->
 				println "printFolderSizes process folder ${node.@folder}"
 				if (config.opt.'print-depth' && (node.name().split(node.@folder.separator.toString()).size() <= config.opt.'print-depth'.toInteger()) ){
-					println "<<${node.name()}>>: SelfSize: ${node.@size}; treeSize: ${node.depthFirst().sum { it.@size }}; treeChilds: ${node.depthFirst().size()}"
+					println "<<${node.name()}>>: SelfSize: ${node.@size}; subTreeSize: ${node.depthFirst().sum { it.@size }}; childSubtreeFolders: ${node.depthFirst().size() - 1}"
 				}
 				false
 			}
@@ -54,10 +56,12 @@ config{
 		)
 		eachMessage = new Operation(
 			folderProcess: {Node node->
-				println "eachMessage process folder <<${node.name()}>>: SelfSize: ${node.@size}; treeSize: ${node.depthFirst().sum { it.@size }}; treeChilds: ${node.depthFirst().size()}"
+				println "eachMessage process folder <<${node.name()}>>: SelfSize: ${node.@size}; subTreeSize: ${node.depthFirst().sum { it.@size }}; childSubtreeFolders: ${node.depthFirst().size() - 1}"
 				true
 			}
-			,messageProcess: {Message m-> println "eachMessage msg SUBJECT: ${m.subject}"} // Is not used in particular case (false returned from folder handler), but for example may contain: println m.subject
+			,messageProcess: {Message m->
+				println "<<${m.folder}>> (Folder attributes: ${m.folder.getAttributes()})); (Labels: ${m.getLabels()}); {SUBJ: ${m.subject}}"
+			}
 		)
 		// Just bing results in interactive GUI GroovyConsole for further experiments
 		GroovyConsole = new Operation(
@@ -77,12 +81,12 @@ config{
 def operation = config.operations.printFolderSizes.clone()
 
 operation.folderProcess = {node->
-    config.operations.printFolderSizes.folderProcess(node) // Call default handler, but also enable messages processing
-    true
+	config.operations.printFolderSizes.folderProcess(node) // Call default handler, but also enable messages processing
+	true
 }
 // Add message processing:
 operation.messageProcess = {m->
-    println "msg SUBJ: ${m.subject}"
+	println "msg SUBJ: ${m.subject}"
 }
 imapTree.traverseTree(operation);
 ''';
@@ -101,6 +105,62 @@ imapTree.traverseTree(operation);
 						}
 					}
 				}
+			}
+		)
+		/**
+		* Task to really delete messages from selected folder, free space (do not leave it in '[Gmail]All mail' archive) and
+		* also do not delete copies from other folders (labels).
+		*
+		* GMail have special settings in options how to handle deleted via IMAP messages. But unfortunately it fully ignored.
+		* See file devel/Delete.Tests for more details and experiments.
+		* Handling IMAP actions also specific: https://support.google.com/mail/answer/77657?hl=en
+		* Really there not so many possibilities:
+		 * 1) Setup move messages to '[Gmail]/Trash' folder and delete messages *without* hold Shift key - then messages fully
+		 * deleted from ALL folders if there was copies.
+		 * 1.1) It does not work with folders - labels like '[Gmail]/Корзина/My deleted folder' created instead.
+		 * 2) If use Shift key or do not move messages into '[Gmail]/Trash' messages appeared in '[Gmail]All mail' and still eat space.
+		 * 3) Deleting label from Gmail web-interface also leave messages in '[Gmail]All mail'
+		 *
+		 * So this task solve that problem and work in next way:
+		 * 1) For configured root folder walk recursively and for each message:
+		 * 2) If it present in more than just that folder - just remove from that folder (delete label)
+		 * 3) If it does not present in any other folders - move it into '[Gmail]/Trash' (localized version should be handled)
+		 * correctly.
+		 * Essentially space from messages must be freed.
+		 *
+		 * Folders leaved as is to examine its empty.
+		**/
+		gmailTrueDeleteMessages = new Operation(
+			folderOpenMode: Folder.READ_WRITE
+			,folderProcess: {Node node->
+				true
+			}
+			,messageProcess: {Message m->
+				println "gmailTrueGeleteMessages: <<${m.folder}>> (Folder attributes: ${m.folder.getAttributes()})); (Labels: ${m.getLabels()}); {SUBJ: ${m.subject}}"
+				List labels = m.getLabels() - ''; // Strange bu always present empty '' label
+				if (labels){ // If their any labels (current folder is not returned in list) - we just remove that - what is equal label remove - it should became in all other
+					println "\tMessage has other labels ${labels} - regular delete it (remove label)"
+					try{
+						m.setFlag(Flags.Flag.DELETED, true);
+					}
+					catch(Exception e){
+						println '!!!ERROR set DELETED flag on message: ' + e
+					}
+				}
+				else{ // otherwise - move (copy) to [Gmail]/Trash folder to fully remove it, even from "[Gmail]/All mail" folder, because settings which are present in settings of account does not work as expected
+					def trashFolder = m.folder.store.getFolder('[Gmail]').list('*').find{ /\Trash/ in it.getAttributes() } // [Gmail]/Корзина in Russian. Locale agnostic search by http://stackoverflow.com/a/26591696/307525
+					println "\tMessage has no other labels, so move to '[Gmail]/Trash' ('${trashFolder}') to do not leave it also in '[Gmail]/All mail' (archive folder)"
+					try{
+						// http://www.jguru.com/faq/view.jsp?EID=1010890 - expunge in folder close later (IMAP have no move operation in base spec http://stackoverflow.com/questions/122267/imap-how-to-move-a-message-from-one-folder-to-another)
+						m.folder.copyMessages((m as javax.mail.Message[]), trashFolder);
+					}
+					catch(Exception e){
+						println '!!!ERROR copy message to [Gmail]/Trash folder: ' + e
+					}
+				}
+			}
+			,folderClose: {node->
+				node.@folder.close(true); // Close and expunge
 			}
 		)
 	}
